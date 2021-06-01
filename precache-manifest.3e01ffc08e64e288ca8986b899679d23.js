36f17689a7469269ef7fdd02c383450057009ef3
Brian Ojoh Mile <brianmile10@yahoo.com>
2021-06-01 11:36:57 -0400

    Updates

---
+++ precache-manifest.3e01ffc08e64e288ca8986b899679d23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a252917f04eb68d49f821f8a55210b2",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "70490868c5f6c9dcfc09",
+    "url": "/simple-calculator/static/css/main.3cd00059.chunk.css"
+  },
+  {
+    "revision": "a2bc51b273f71036f196",
+    "url": "/simple-calculator/static/js/2.df802cda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-calculator/static/js/2.df802cda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70490868c5f6c9dcfc09",
+    "url": "/simple-calculator/static/js/main.6bc1d930.chunk.js"
+  },
+  {
+    "revision": "b82fd816b6d849d7a823",
+    "url": "/simple-calculator/static/js/runtime-main.b0062540.js"
+  },
+  {
+    "revision": "9f7ddf02db3bd4e51c850087aab34dd5",
+    "url": "/simple-calculator/static/media/background.9f7ddf02.png"
+  }
+]);
